25ccf0ce4e98d0460066e3c05481621d0436e381
Simon Huang <firebfm@gmail.com>
2021-01-07 02:48:27 +0000

    name of teachers that assisted -toggle answer

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -15,11 +15,11 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
 ORDER BY teacher;
 `)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   })
-}).catch(err => console.error('query error', err.stack));
+});
