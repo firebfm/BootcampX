4da4415e54e6151f9839725db5631d8506b5645d
Simon Huang <firebfm@gmail.com>
2021-01-07 01:59:56 +0000

    create new pool, connect, query res.rows

---
+++ bootcamp_app/students.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.connect();
+
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  console.log(res.rows);
+})
+.catch(err => console.error('query error', err.stack));
